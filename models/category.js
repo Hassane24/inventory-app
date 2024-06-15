e6fe0bd869a2bd238b8ee24693e62564f1ff7b17
Hassane <hsisane5@gmail.com>
2024-06-15 11:57:32 +0100

    added missing export statement

--- models/category.js
+++ models/category.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const categorySchema = new Schema({
+const CategorySchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
 });
 
-categorySchema.virtual("url").get(function () {
+CategorySchema.virtual("url").get(function () {
   return `/categories/${this._id}`;
 });
+module.exports = mongoose.model("Category", CategorySchema);
