48080ff55fd1aabfe9f7b5cbb73a6f3e5376d1dd
Hassane <hsisane5@gmail.com>
2024-06-15 11:58:46 +0100

    added a missing export statement and changed data type for release date

--- models/game.js
+++ models/game.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const gameSchema = new Schema({
+const GameSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   synopsis: { type: String, required: true, maxLength: 1000 },
   category: { type: Schema.Types.ObjectId, ref: "Category", require: true },
   rating: { type: Number, required: true, min: 0, max: 10 },
   price: { type: Number, required: true },
-  release_date: { type: Date, required: true },
+  release_date: { type: String, required: true },
 });
 
-gameSchema.virtual("url").get(function () {
+GameSchema.virtual("url").get(function () {
   return `/games/${this._id}`;
 });
+
+module.exports = mongoose.model("Game", GameSchema);
