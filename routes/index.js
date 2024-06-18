3064f9e2207b3b84315c8f3d67dffdf35b245669
Hassane <hsisane5@gmail.com>
2024-06-18 22:14:45 +0100

    made navbar exclusively for home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "GameStop" });
+  res.render("", { title: "GameStop" });
 });
 
 module.exports = router;
