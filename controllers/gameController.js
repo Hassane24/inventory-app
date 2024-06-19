d704101833f55c16bc301ec9f1c503153ba25dad
Hassane <hsisane5@gmail.com>
2024-06-19 22:11:51 +0100

    added a new view for each game

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,10 +1,29 @@
 const Game = require("../models/game");
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 
 exports.game_list = asyncHandler(async (req, res, next) => {
-  const allGames = await Game.find().sort({ name: 1 }).exec();
+  const allGames = await Game.find({}, "name").sort({ name: 1 }).exec();
   res.render("games", {
     title: "Games",
     games: allGames,
   });
+});
+
+exports.game_detail = asyncHandler(async (req, res, next) => {
+  const game = await Game.findById(req.params.id).populate("category").exec();
+  console.log(game);
+  if (game === null) {
+    const err = new Error("Game not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("gamePage", {
+    game: game,
+  });
+});
+
+exports.game_edit_get = asyncHandler(async (req, res, next) => {
+  const [game, categories] = await Promise.all([Game.find]);
 });
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const gameContoller = require("../controllers/gameController");
 router.get("/", gameContoller.game_list);
+router.get("/:id", gameContoller.game_detail);
 
 module.exports = router;
